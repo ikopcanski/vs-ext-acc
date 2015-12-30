668e2c5dfe0c183935893117cf74ff165b67f0e1
ikopcanski <ivan.kopcanski@hotmail.com>
2015-12-31 01:55:24 +0400

    Support for Contract.OldValue replacement is added. Few bugfixes with contract statement replacement strategies.

--- CodeContracts.Contrib/Rewriters/ContractExpressionTransformer.cs
+++ CodeContracts.Contrib/Rewriters/ContractExpressionTransformer.cs
@@ -12,14 +12,14 @@ namespace CodeContracts.Contrib.Rewriters
 {
     internal class ContractExpressionTransformer
     {
-        private List<IContractExpressionStrategy> _strategies;
+        private List<ContractExpressionStrategyBase> _strategies;
         private StatementSyntax _contractCallStatement;
         private StatementSyntax _returnStatement;
         private string _returnVarName;
 
         public ContractExpressionTransformer(StatementSyntax contractCallStatement, StatementSyntax returnStatement, string returnVarName)
         {
-            _strategies = new List<IContractExpressionStrategy>()
+            _strategies = new List<ContractExpressionStrategyBase>()
             {
                 new GenericRequiresExpressionStrategy(),
                 new SimpleRequiresExpressionStrategy(),
@@ -32,24 +32,6 @@ namespace CodeContracts.Contrib.Rewriters
             _returnVarName = returnVarName;
         }
 
-        private Tuple<StatementSyntax, bool> Transform(ExpressionStatementSyntax exp)
-        {
-            StatementSyntax retVal = null;
-            bool isPreCondition = false;
-
-            foreach (var strategy in _strategies)
-            {
-                if (strategy.IsMine(exp))
-                {
-                    retVal = strategy.Transform(exp, _returnVarName);
-                    isPreCondition = strategy.IsPreCondition(exp);
-                    break;
-                }
-            }
-
-            return new Tuple<StatementSyntax, bool>(retVal, isPreCondition);
-        }
-
         /// <summary>
         /// Transforms list of Contract.Requires<T>(...) and/or Contract.Requires(...) statements into 'if' statements that throw corresponding exception. 
         /// Embeds the contract method call statement and return value statement, so result is complete method body with statements.
@@ -58,23 +40,29 @@ namespace CodeContracts.Contrib.Rewriters
         /// <returns>complete method body with statements.</returns>
         public IEnumerable<StatementSyntax> Transform(IEnumerable<ExpressionStatementSyntax> expressions)
         {
+            var oldValueAssignments = new List<StatementSyntax>();
             var preConditionStatements = new List<StatementSyntax>();
             var postConditionStatements = new List<StatementSyntax>();
 
             foreach (var expression in expressions)
             {
-                var statement = Transform(expression);
-                if (statement.Item2)
+                var transform = Transform(expression);
+
+                oldValueAssignments.AddRange(transform.OldValueAssignments);
+
+                if (transform.IsPreCondition)
                 {
-                    preConditionStatements.Add(statement.Item1);
+                    preConditionStatements.Add(transform.Transform);
                 }
                 else
                 {
-                    postConditionStatements.Add(statement.Item1);
+                    postConditionStatements.Add(transform.Transform);
                 }
             }
 
-            var retVal = preConditionStatements.Union(new[] { _contractCallStatement }).Union(postConditionStatements);
+            var retVal = oldValueAssignments.Union(preConditionStatements)
+                                            .Union(new[] { _contractCallStatement })
+                                            .Union(postConditionStatements);
             if (_returnStatement != null)
             {
                 retVal = retVal.Union(new[] { _returnStatement });
@@ -82,28 +70,96 @@ namespace CodeContracts.Contrib.Rewriters
 
             return retVal;          
         }
+
+        private ContractStatementTransform Transform(ExpressionStatementSyntax exp)
+        {
+            ContractStatementTransform retVal = null;
+
+            foreach (var strategy in _strategies)
+            {
+                if (strategy.IsMine(exp))
+                {
+                    retVal = strategy.Transform(exp, _returnVarName);
+                    break;
+                }
+            }
+
+            return retVal;
+        }
     }
 
-    internal interface IContractExpressionStrategy
+    internal class ContractStatementTransform
     {
-        int Priority { get; }
-        bool IsMine(ExpressionStatementSyntax exp);
-        bool IsPreCondition(ExpressionStatementSyntax exp);
-        StatementSyntax Transform(ExpressionStatementSyntax exp, string returnVarName);
+        public StatementSyntax Transform { get; set; }
+
+        public bool IsPreCondition { get; set; }
+
+        public IEnumerable<StatementSyntax> OldValueAssignments { get; set; }
     }
 
-    internal class GenericRequiresExpressionStrategy : IContractExpressionStrategy
+    internal abstract class ContractExpressionStrategyBase
     {
-        public int Priority { get { return 100; } }
+        public abstract int Priority { get; }
+        public abstract bool IsMine(ExpressionStatementSyntax exp);
+        public abstract ContractStatementTransform Transform(ExpressionStatementSyntax exp, string returnVarName);
 
-        public bool IsMine(ExpressionStatementSyntax exp)
+        protected IEnumerable<string> ExtractValidations(string condition)
+        {
+            var matches = Regex.Matches(condition, @"Contract.OldValue<\w+>\(\w+\)");
+            foreach (Match oldValueMatch in matches)
+            {
+                yield return oldValueMatch.Value;
+            }
+            matches = Regex.Matches(condition, @"Contract.OldValue\(\w+\)");
+            foreach (Match oldValueMatch in matches)
+            {
+                yield return oldValueMatch.Value;
+            }
+        }
+
+        protected string ReplaceValidations(string condition, IEnumerable<string> validations)
+        {
+            var retVal = condition;
+
+            foreach (var validation in validations)
+            {
+                var idendifier = Regex.Match(validation, @"\(\w+\)").Value.Trim('(', ')');
+                
+                if (validation.StartsWith("Contract.OldValue"))
+                {
+                    var oldValue = string.Format("{0}_old", idendifier);
+                    retVal = retVal.Replace(validation, oldValue);
+                }
+            }
+
+            return retVal;
+        }
+
+        protected IEnumerable<StatementSyntax> GetOldValueAssignments(IEnumerable<string> validations)
+        {
+            foreach (var validation in validations)
+            {
+                var idendifier = Regex.Match(validation, @"\(\w+\)").Value.Trim('(', ')');
+
+                if (validation.StartsWith("Contract.OldValue"))
+                {
+                    var assignment = string.Format("var {0}_old = {1};", idendifier, idendifier);
+                    yield return SyntaxFactory.ParseStatement(assignment);
+                }
+            }
+        }
+    }
+
+    internal class GenericRequiresExpressionStrategy : ContractExpressionStrategyBase
+    {
+        public override int Priority { get { return 100; } }
+
+        public override bool IsMine(ExpressionStatementSyntax exp)
         {
             return exp.Str().StartsWith("Contract.Requires<");
         }
 
-        public bool IsPreCondition(ExpressionStatementSyntax exp) { return true; }
-
-        public StatementSyntax Transform(ExpressionStatementSyntax exp, string returnVarName)
+        public override ContractStatementTransform Transform(ExpressionStatementSyntax exp, string returnVarName)
         {
             var exceptionType = exp.FirstChild<GenericNameSyntax>()
                                    .ChildrenOfType<IdentifierNameSyntax>()
@@ -112,6 +168,10 @@ namespace CodeContracts.Contrib.Rewriters
             var argumentsList = exp.FirstChild<ArgumentListSyntax>();
 
             var condition = argumentsList.Arguments.FirstOrDefault().Str();
+
+            var validations = ExtractValidations(condition);
+
+            condition = ReplaceValidations(condition, validations);
 
             var message = string.Format("\"Precondition ({0}) is not satisfied!\"", condition.Replace('"', '\''));
             
@@ -122,26 +182,33 @@ namespace CodeContracts.Contrib.Rewriters
 
             var statementStr = string.Format("if (!({0})) \r\n {{ \r\n throw new {1}({2}); \r\n }}", condition, exceptionType, message);
 
-            return SyntaxFactory.ParseStatement(statementStr);
+            return new ContractStatementTransform
+            {
+                Transform = SyntaxFactory.ParseStatement(statementStr),
+                IsPreCondition = !exp.Str().Contains("Contract.Result"),
+                OldValueAssignments = GetOldValueAssignments(validations)
+            };
         }
     }
 
-    internal class SimpleRequiresExpressionStrategy : IContractExpressionStrategy
+    internal class SimpleRequiresExpressionStrategy : ContractExpressionStrategyBase
     {
-        public int Priority { get { return 80; } }
+        public override int Priority { get { return 80; } }
         
-        public bool IsMine(ExpressionStatementSyntax exp)
+        public override bool IsMine(ExpressionStatementSyntax exp)
         {
             return exp.Str().StartsWith("Contract.Requires(");
         }
 
-        public bool IsPreCondition(ExpressionStatementSyntax exp) { return true; }
-
-        public StatementSyntax Transform(ExpressionStatementSyntax exp, string returnVarName)
+        public override ContractStatementTransform Transform(ExpressionStatementSyntax exp, string returnVarName)
         {
             var argumentsList = exp.FirstChild<ArgumentListSyntax>();
 
             var condition = argumentsList.Arguments.FirstOrDefault().Str();
+
+            var validations = ExtractValidations(condition);
+
+            condition = ReplaceValidations(condition, validations);
 
             var message = string.Format("\"Precondition ({0}) is not satisfied!\"", condition.Replace('"', '\''));
 
@@ -152,28 +219,35 @@ namespace CodeContracts.Contrib.Rewriters
 
             var statementStr = string.Format("if (!({0})) \r\n {{ \r\n throw new Exception({1}); \r\n }}", condition, message);
 
-            return SyntaxFactory.ParseStatement(statementStr);
+            return new ContractStatementTransform
+            {
+                Transform = SyntaxFactory.ParseStatement(statementStr),
+                IsPreCondition = !exp.Str().Contains("Contract.Result"),
+                OldValueAssignments = GetOldValueAssignments(validations)
+            };
         }
     }
 
-    internal class EnsuresExpressionStrategy : IContractExpressionStrategy
+    internal class EnsuresExpressionStrategy : ContractExpressionStrategyBase
     {
-        public int Priority { get { return 60; } }
+        public override int Priority { get { return 60; } }
 
-        public bool IsMine(ExpressionStatementSyntax exp)
+        public override bool IsMine(ExpressionStatementSyntax exp)
         {
             return exp.Str().StartsWith("Contract.Ensures(");
         }
 
-        public bool IsPreCondition(ExpressionStatementSyntax exp) { return false; }
-
-        public StatementSyntax Transform(ExpressionStatementSyntax exp, string returnVarName)
+        public override ContractStatementTransform Transform(ExpressionStatementSyntax exp, string returnVarName)
         {
             var argumentsList = exp.FirstChild<ArgumentListSyntax>();
 
             var condition = argumentsList.Arguments.FirstOrDefault().Str();
 
             condition = Regex.Replace(condition, @"Contract.Result<\w+>\(\)", returnVarName);
+
+            var validations = ExtractValidations(condition);
+
+            condition = ReplaceValidations(condition, validations);
 
             var message = string.Format("\"Postcondition ({0}) is not satisfied!\"", condition.Replace('"', '\''));
 
@@ -184,21 +258,31 @@ namespace CodeContracts.Contrib.Rewriters
 
             var statementStr = string.Format("if (!({0})) \r\n {{ \r\n throw new Exception({1}); \r\n }}", condition, message);
 
-            return SyntaxFactory.ParseStatement(statementStr);
+            return new ContractStatementTransform
+            {
+                Transform = SyntaxFactory.ParseStatement(statementStr),
+                IsPreCondition = false,
+                OldValueAssignments = GetOldValueAssignments(validations)
+            };
         }
     }
 
-    internal class DefaultExpressionStrategy : IContractExpressionStrategy
+    internal class DefaultExpressionStrategy : ContractExpressionStrategyBase
     {
-        public int Priority { get { return 0; } }
+        public override int Priority { get { return 0; } }
 
-        public bool IsMine(ExpressionStatementSyntax exp) { return exp.Str().StartsWith("Contract."); ; }
-
-        public bool IsPreCondition(ExpressionStatementSyntax exp) { return true; }
-
-        public StatementSyntax Transform(ExpressionStatementSyntax exp, string returnVarName)
+        public override bool IsMine(ExpressionStatementSyntax exp) { return exp.Str().StartsWith("Contract."); ; }
+        
+        public override ContractStatementTransform Transform(ExpressionStatementSyntax exp, string returnVarName)
         {
-            return exp;
+            return new ContractStatementTransform
+            {
+                Transform = exp,
+                IsPreCondition = true,
+                OldValueAssignments = new StatementSyntax[0]
+            };
         }
     }
+
+    
 }

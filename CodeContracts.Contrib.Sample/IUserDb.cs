87dc635f1ad64a267fe38a4a78bb52cb2b90bafe
ikopcanski <ivan.kopcanski@hotmail.com>
2016-01-16 23:13:13 +0400

    Visual Studio MPF dependency version is downgrated to 12.0

--- CodeContracts.Contrib.Sample/IUserDb.cs
+++ CodeContracts.Contrib.Sample/IUserDb.cs
@@ -2,7 +2,7 @@ using System.Diagnostics.Contracts;
 
 namespace CodeContracts.Contrib.Sample
 {
-    [ContractClass(typeof (IUserDb_Contract))]
+    
     public interface IUserDb
     {
         int SaveUser(UserDto user, bool isUpdate);
